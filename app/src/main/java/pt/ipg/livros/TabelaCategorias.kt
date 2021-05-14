d5a077dd9e7b621ca882dfd1ff1c9d8c25cc01a2
Leonardo Marques <leonardo2000marques@gmail.com>
2021-05-14 09:49:50 +0100

    Implementar operações CRUD da tabela categorias

--- app/src/main/java/pt/ipg/livros/TabelaCategorias.kt
+++ app/src/main/java/pt/ipg/livros/TabelaCategorias.kt
@@ -1,5 +1,7 @@
 package pt.ipg.livros
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.provider.BaseColumns
 
@@ -7,6 +9,39 @@ class TabelaCategorias(db: SQLiteDatabase) {
     private val db: SQLiteDatabase = db
 
     fun cria() {
-        db.execSQL("CREATE TABLE categorias(_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE " + NOME_Tabela+
+                "(" + BaseColumns._ID+
+                " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                CAMPO_NOME+ "TEXT NOT NULL)")
+    }
+
+    // CRUD
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_Tabela, null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int{
+        return db.update(NOME_Tabela, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_Tabela, whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_Tabela, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_Tabela = "CATEGORIAS"
+        const val CAMPO_NOME = "nome"
     }
 }
